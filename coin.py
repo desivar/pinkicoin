b86f4b2907379f6bc79020c87302eccf51ca2797
desivar <jillian_desire@yahoo.es>
2025-05-03 15:07:58 -0600

    fix typo in images

--- coin.py
+++ coin.py
@@ -5,6 +5,6 @@ class coin(pygame.sprite.Sprite):
     def __init__(self, pos):
         super().__init__()
         self.image = pygame.image.load(
-            "assets/images/coin.png"
+            "assets/images/pinkygirl.jpj"
         ).convert_alpha()
         self.rect = self.image.get_rect(center=pos)
