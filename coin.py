4359a031e0bbaae6dcd7863ee76bdcfec919c792
desivar <jillian_desire@yahoo.es>
2025-05-03 11:47:59 -0600

    get rid of extra characters adding new lines

--- coin.py
+++ coin.py
@@ -1,9 +1,10 @@
-# coin.py
 import pygame
 
 
 class Coin(pygame.sprite.Sprite):
     def __init__(self, pos):
         super().__init__()
-        self.image = pygame.image.load("assets/images/coin.png").convert_alpha()
+        self.image = pygame.image.load(
+            "assets/images/coin.png"
+        ).convert_alpha()
         self.rect = self.image.get_rect(center=pos)
